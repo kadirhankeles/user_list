53bc32ba5e4599079c40084625240be52c1e69c4
Kadirhan Keleş <kadirhankeles@gmail.com>
2022-11-17 21:20:34 +0300

    app complated

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -14,11 +14,12 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
     // TODO: implement initState
-    
+
     userProvider = Provider.of<UserProvider>(context, listen: false);
     userProvider!.getData(context);
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,54 +31,68 @@ class _HomeScreenState extends State<HomeScreen> {
           return Padding(
             padding: const EdgeInsets.all(8.0),
             child: Column(
-            children: [
-              Container(
-                height: 700,
-                width: double.infinity,
-                child: GridView.builder(
-                  itemCount: value.response.data!.length,
-                  gridDelegate:
-                      SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisSpacing: 10,
-                      mainAxisSpacing: 10,
-                      crossAxisCount: 2,
-                      childAspectRatio: 1/1.3),
-                  itemBuilder: (context, index) {
-                    
-                    return Container(
-                      decoration: BoxDecoration(border: Border.all(color: Colors.grey, width: 0.4)),
-                      child: Column(crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Center(
-                            child: Padding(
-                              padding: const EdgeInsets.only(top: 20, bottom: 10),
-                              child: Container(
-                                height: 150,
-                                width: 150,
-                                decoration: BoxDecoration(image: DecorationImage(image: NetworkImage("${value.response.data![index].avatar}"),fit: BoxFit.cover)),
+              children: [
+                Container(
+                  height: 700,
+                  width: double.infinity,
+                  child: GridView.builder(
+                    itemCount: value.response.data!.length,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisSpacing: 10,
+                        mainAxisSpacing: 10,
+                        crossAxisCount: 2,
+                        childAspectRatio: 1 / 1.2),
+                    itemBuilder: (context, index) {
+                      return Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              border:
+                                  Border.all(color: Colors.grey, width: 0.4)),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Center(
+                                child: Padding(
+                                  padding: const EdgeInsets.only(
+                                      top: 15, bottom: 10),
+                                  child: Container(
+                                    height: 120,
+                                    width: 120,
+                                    decoration: BoxDecoration(
+                                        image: DecorationImage(
+                                            image: NetworkImage(
+                                                "${value.response.data![index].avatar}"),
+                                            fit: BoxFit.cover),
+                                        borderRadius: BorderRadius.circular(100)    
+                                            ),
+                                  ),
+                                ),
                               ),
-                              
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 20, top: 10),
-                            child: Text("${value.response.data![index].firstName} ${value.response.data![index].lastName}",style: TextStyle(fontWeight: FontWeight.bold),),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 20, top: 10),
-                            child: Text("${value.response.data![index].email}", style: TextStyle(fontSize: 12),),
-                          ),
-                        ],
-                      ) 
-                      );
-                  },
-                ),
-              )
-            ],
-        ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.only( top: 10),
+                                child: Text(
+                                  "${value.response.data![index].firstName} ${value.response.data![index].lastName}",
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.only(top: 10),
+                                child: Text(
+                                  "${value.response.data![index].email}",
+                                  style: TextStyle(fontSize: 12),
+                                ),
+                              ),
+                            ],
+                          ));
+                    },
+                  ),
+                )
+              ],
+            ),
           );
         },
-
       ),
     );
   }
